1294a0a3ecd24529c8e9d4f2709a4636bdc5d718
Bowen Zhang <bz2306@nyu.edu>
2023-07-27 01:40:55 -0400

    Implement albums and song details on Playlist Page

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -2,70 +2,52 @@ package com.laioffer.spotify
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.laioffer.spotify.ui.theme.SpotifyTheme
+import com.laioffer.spotify.network.NetworkApi
+import com.laioffer.spotify.network.NetworkModule
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-
-// customized extend AppCompatActivity
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var api: NetworkApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SpotifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
 
         setContentView(R.layout.activity_main)
 
         val navView = findViewById<BottomNavigationView>(R.id.nav_view)
 
+        // controller
         val navHostFragment =supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
         val navController = navHostFragment.navController
-        navController.setGraph(R.navigation.nav_graph)
 
+        // data model
+        navController.setGraph(R.navigation.nav_graph)
         NavigationUI.setupWithNavController(navView, navController)
 
         // https://stackoverflow.com/questions/70703505/navigationui-not-working-correctly-with-bottom-navigation-view-implementation
-        navView.setOnItemSelectedListener{
-            NavigationUI.onNavDestinationSelected(it, navController)
-            navController.popBackStack(it.itemId, inclusive = false)
+        navView.setOnItemSelectedListener{ menuItem ->
+            NavigationUI.onNavDestinationSelected(menuItem, navController)
+            navController.popBackStack(menuItem.itemId, inclusive = false)
             true
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SpotifyTheme {
-        Surface {
-            Greeting("Android")
+        // Test retrofit
+        GlobalScope.launch(Dispatchers.IO) {
+            // comment out
+            val api = NetworkModule.provideRetrofit().create(NetworkApi::class.java)
+            val response = api.getHomeFeed().execute().body()
+            Log.d("Network", response.toString())
         }
     }
 }
